cb97f3052cbdc92085925d84fbe29c94471ac9cf
nrht <i.norihi.472@gmail.com>
2021-02-16 02:38:41 +0900

    implemented minimum microservices

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 export default () => {
     const [posts, setPosts] = useState({});
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
+        const res = await axios.get('http://localhost:4002/posts');
         setPosts(res.data);
     };
-
     useEffect(() => {
         fetchPosts();
     }, []);
@@ -22,6 +22,7 @@ export default () => {
             >
                 <div className="card-body">
                     <h3>{post.title}</h3>
+                    <CommentList comments={post.comments} />
                     <CommentCreate postId={post.id} />
                 </div>
             </div>
