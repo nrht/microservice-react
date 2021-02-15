2a58687c91551698fd625ec02c5315901012a24e
nrht <i.norihi.472@gmail.com>
2021-02-15 17:06:28 +0900

    implemented app skelton

--- comments/index.js
+++ comments/index.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
-const posts = {};
+const commentsByPostId = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
+const comments = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.paramas.id] || []);
 });
 
-app.post('/posts', (req, res) => {
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
-    posts[id] = {
-        id, title
-    };
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
 
-    res.status(201).send(posts[id]);
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: commentId, content});
+    commentsByPostId[req.params.id] = comments;
+    res.status(201).send(comments);
 });
 
-app.listen(4000, () => {
-    console.log('Listening on 4000');
-});
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+})
