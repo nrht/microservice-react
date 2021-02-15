8490ef3656958aa2214bd62c4897927a1985aac4
nrht <i.norihi.472@gmail.com>
2021-02-16 03:40:11 +0900

    dockernizing post service

--- event-bus/index.js
+++ event-bus/index.js
@@ -17,6 +17,9 @@ app.post('/events', (req, res) => {
     axios.post('http://localhost:4002/events', event);
     axios.post('http://localhost:4003/events', event);
 
+    axios.post('http://localhost:4006/events', event);
+    axios.post('http://localhost:4007/events', event);
+
     res.send({ status: 'OK' });
 });
 
